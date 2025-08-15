2625de25e6499c4b52ea4184af7b7fd3383508f2
Tuấn Khang <khangkttb01029@fpt.edu.vn>
2025-08-15 09:35:57 +0700

    AddUser

--- Asm_GD1/Controllers/UserAdminController.cs
+++ Asm_GD1/Controllers/UserAdminController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Asm_GD1.Data;
+using Asm_GD1.Models;
+using Asm_GD1.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Asm_GD1.Controllers
 {
     [Authorize(Roles = "admin, adminit")]
     public class UserAdminController : Controller
     {
+        private readonly AppDbContext _context;
+        public UserAdminController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Dashboard()
         {
             return View();
@@ -34,6 +44,41 @@ namespace Asm_GD1.Controllers
         public IActionResult Settings()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Account account)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(account);
+            }
+
+            var hasher = new PasswordHasher<Account>();
+            var acc = new Account
+            {
+                Username = account.Username,
+                Email = account.Email,
+                PhoneNumber = account.PhoneNumber,
+                Role = account.Role,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now
+            };
+
+            acc.Password = hasher.HashPassword(acc, account.Password);
+
+            _context.Accounts.Add(acc);
+            await _context.SaveChangesAsync();
+
+            TempData["success"] = "Thêm tài khoản thành công";
+            return RedirectToAction("UserManagement");
         }
     }
 }
